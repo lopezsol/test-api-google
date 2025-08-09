352555d6f92f14db069a25c4a35d81e7e93be44e
lopezsol <mariasollopez23@gmail.com>
2025-08-09 18:01:31 -0300

    se modifica login para obtener el access_token. Se agregan eventos de calendar

--- src/app/shared/pages/home-page/home-page.component.ts
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -1,24 +1,28 @@
 import { Component, computed, signal } from '@angular/core';
+import { CalendarComponent } from "../../../calendar/components/calendar/calendar.component";
 
 @Component({
   selector: 'home-page',
-  imports: [],
+  imports: [CalendarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
 export class HomePageComponent {
-  token = signal<string>(sessionStorage.getItem('token')!);
-  user = computed(() => this.decodeJwtPayload(this.token()));
+  // token = signal<string>(sessionStorage.getItem('token')!);
+  // token = signal<string>(sessionStorage.getItem('access_token')!);
 
-  decodeJwtPayload(token: string): any {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(
-      atob(base64)
-        .split('')
-        .map((c) => `%${('00' + c.charCodeAt(0).toString(16)).slice(-2)}`)
-        .join('')
-    );
-    return JSON.parse(jsonPayload);
-  }
+  // user = computed(() => this.decodeJwtPayload(this.token()));
+
+  // decodeJwtPayload(token: string): any {
+  //   console.log("entre en el decode", token)
+  //   const base64Url = token.split('.')[1];
+  //   const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  //   const jsonPayload = decodeURIComponent(
+  //     atob(base64)
+  //       .split('')
+  //       .map((c) => `%${('00' + c.charCodeAt(0).toString(16)).slice(-2)}`)
+  //       .join('')
+  //   );
+  //   return JSON.parse(jsonPayload);
+  // }
 }
--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,3 +1,5 @@
 export const environment = {
   googleClientId: 'TU_CLIENT_ID_DEV.apps.googleusercontent.com',
+  googleApiKey: 'TU_CLAVE_API'
+
 };
