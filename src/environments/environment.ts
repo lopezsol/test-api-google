f91b7addafda4a5179ed764ce643a5948683a7b4
lopezsol <mariasollopez23@gmail.com>
2025-08-12 23:09:29 -0300

    se saca environment.ts del gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  googleClientId: 'TU_CLIENT_ID_DEV.apps.googleusercontent.com',
+  googleApiKey: 'TU_CLAVE_API',
+  mobyUrl: 'URL_MOBY',
+  redirectUriLogin: 'URI_LOGIN',
+};
