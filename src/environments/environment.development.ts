03ba5083e6797895bbb2898c28f820c2c4ec0363
Julian Mauro Pellegrini <pellegrinijulianmauro@gmail.com>
2025-08-29 12:37:37 -0300

    environment agregado

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  googleClientId:
+    '209813369626-29rh4hsrn1t36e2014f7oqnfecjrat54.apps.googleusercontent.com',
+  googleApiKey: 'AIzaSyCO5sJ3wfB-VHJIJNvI2bRBuV4SRuHkbhg',
+  mobyUrl: 'http://localhost:8080/api',
+  redirectUriLogin: 'http://localhost:8080/api/auth/google/callback',
+  // mobyUrl: 'https://cf3f5da9f29c.ngrok-free.app/api',
+};
+
